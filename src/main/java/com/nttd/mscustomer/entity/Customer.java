480e8d784fb7a1af6f57a495bc4d39030e225abb
Alfonso Victor Cespedes <alfonsocespedes@primacia.onmicrosoft.com>
2023-02-14 09:39:36 -0500

    agregando id unico en las tablas v1

--- src/main/java/com/nttd/mscustomer/entity/Customer.java
+++ src/main/java/com/nttd/mscustomer/entity/Customer.java
@@ -2,7 +2,9 @@ package com.nttd.mscustomer.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +14,12 @@ import lombok.Setter;
 public class Customer {
 
 	@Id
-	@GeneratedValue
+	@SequenceGenerator(
+            name = "customerSequence",
+            sequenceName = "customer_id_seq",
+            allocationSize = 1,
+            initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customerSequence") 
 	private Long idCustomer;
 	private String typeCustomer;
 	private String numberDocument;
