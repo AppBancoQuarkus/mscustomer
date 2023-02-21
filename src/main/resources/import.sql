e695a38c9ee0ff5b74bd0e2c17a7f92ed28fb4fd
Alfonso Victor Cespedes <alfonsocespedes@primacia.onmicrosoft.com>
2023-02-21 10:42:38 -0500

    modificando de importacion

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,7 +5,7 @@ VALUES(2, 'ROJAS', 'LUCAS', '48521639', 'A', 'P');
 INSERT INTO public.bqmcustomer(idcustomer, lastname, "name", numberdocument, state, typecustomer)
 VALUES(3, 'PERALDA', 'LUIS', '69852314', 'A', 'P');
 INSERT INTO public.bqmcustomer(idcustomer, lastname, "name", numberdocument, state, typecustomer)
-VALUES(4, 'GIL', 'RIO', '75412369', 'A', 'P');
+VALUES(4, 'DANTE', 'ANTHONY', '75412369', 'A', 'P');
 INSERT INTO public.bqmcustomer(idcustomer, lastname, "name", numberdocument, state, typecustomer)
 VALUES(5, '', 'EMPRESA PEPITO SAC', '20452136987', 'A', 'E');
 INSERT INTO public.bqmcustomer(idcustomer, lastname, "name", numberdocument, state, typecustomer)
